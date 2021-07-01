8642d52ede245853217a7d4c7eadee2779feebc0
hennneh <hendrik.lehmann96@gmail.com>
2021-07-01 16:42:52 +0200

    testing va_list

--- test.c
+++ test.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
+#include <stdarg.h>
+
+void arg_func(int i, ...)
+{
+	va_list someList;
+	va_start(someList, i);
+	int j = 0;
+	int sum = 0;
+	while (j < i)
+	{
+		 sum += va_arg(someList, int);
+	}
+	va_end(someList);
+	printf("%d\n", sum);
+}
 
 int	main(void)
 {
-	int i = 48;
-	printf("%c\n", i);
+	printf("beginngin of program\n");
+	arg_func(3, 1, 1, 1);
+	printf("function has been called\n");
+	return (0);
 }
