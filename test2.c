2379a458b19fd11486242239ea9f94a0067df733
hennneh <hendrik.lehmann96@gmail.com>
2021-07-02 21:33:43 +0200

    testing struct

--- test2.c
+++ test2.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+typedef struct
+{
+	int length;
+	int	width;
+} rectangle;
+
 int main(void)
 {
-	printf("%c\n", 749);
+	rectangle myRectangle = {5, 10};
+	printf("length = %d\nwidth = %d\n", myRectangle.length, myRectangle.width);
 	return (0);
 }
