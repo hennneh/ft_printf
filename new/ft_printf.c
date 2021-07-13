b7f0c073434c0871ba6e366c9a48b2866cb95d32
hennneh <hendrik.lehmann96@gmail.com>
2021-07-13 13:42:01 +0200

    adding tests to makefile

--- new/ft_printf.c
+++ new/ft_printf.c
@@ -1,10 +1,35 @@
 #include "ft_printf.h"
 
-int	ft_printf(char *fmt, ...)
+int	check_flags(va_list args, const char *fmt, int *i)
+{
+	(*i)++;
+	if (fmt[*i] == '%')
+	{
+		write(1, "%", 1);
+		return (1);
+	}
+	return (0);
+}
+int	ft_printf(const char *fmt, ...)
 {
 	va_list	args;
 	int		printed;
+	int		i;
 
 	va_start(args, fmt);
 	printed = 0;
+	i = 0;
+	while (fmt[i] != '\0')
+	{
+		if (fmt[i] == '%')
+			printed += check_flags(args, fmt, &i);
+		else
+		{
+			write(1, &fmt[i], 1);
+			i++;
+			printed++;
+		}
+	}
+	va_end(args);
+	return (printed);
 }
---
+++ new/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int     ft_printf(const char *fmt, ...);
+
+int	main(void)
+{
+	ft_printf("hello you\n");
+	return (0);
+}
