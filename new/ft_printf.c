c9320ea8691981a6782210b55813b88fc4adc4a3
hennneh <hendrik.lehmann96@gmail.com>
2021-07-13 09:56:20 +0200

    core changes

---
+++ new/ft_printf.c
@@ -0,0 +1,10 @@
+#include "ft_printf.h"
+
+int	ft_printf(char *fmt, ...)
+{
+	va_list	args;
+	int		printed;
+
+	va_start(args, fmt);
+	printed = 0;
+}
---
+++ new/ft_printf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hlehmann <hlehmann@student.42wolfsburg.de> +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/13 16:54:48 by hlehmann          #+#    #+#             */
+/*   Updated: 2021/05/13 16:57:26 by hlehmann         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <unistd.h>
+# include <stdarg.h>
+
+int	ft_printf(const char *fmt, ...);
+
+#endif
