9598015a85de7743c2c0bc33551eef3f431723aa
hennneh <hendrik.lehmann96@gmail.com>
2021-08-23 17:53:36 +0200

    adding libft-files

--- v5/ft_printf.h
+++ v5/ft_printf.h
@@ -16,13 +16,17 @@
 
 # include <stdarg.h>
 
-int	ft_printf(const char *format, ...);
-int	print_c(va_list *arguments);
-int	print_s(va_list *arguments);
-int	print_p(va_list *arguments);
-int	print_i(va_list *arguments);
-int	print_u(va_list *arguments);
-int	print_x_lo(va_list *arguments);
-int	print_x_up(va_list *arguments);
+int		ft_printf(const char *format, ...);
+int		print_c(va_list *arguments);
+int		print_s(va_list *arguments);
+int		print_p(va_list *arguments);
+int		print_i(va_list *arguments);
+int		print_u(va_list *arguments);
+int		print_x_lo(va_list *arguments);
+int		print_x_up(va_list *arguments);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_putstr_fd(char *s, int fd);
+size_t	ft_strlen(const char *str);
 
 #endif
---
+++ v5/utils.c
@@ -0,0 +1,51 @@
+#include "ft_printf.h"
+
+void	ft_putchar_fd(char c, int fd)
+{
+	write(fd, &c, 1);
+}
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	char	c;
+
+	if (n < 0)
+		write(fd, "-", 1);
+	if (n == -2147483648)
+	{
+		write(fd, "2", 1);
+		n = -147483648;
+	}
+	if (n < 0)
+		n *= -1;
+	if (n / 10 > 0)
+		ft_putnbr_fd(n / 10, fd);
+	c = n % 10 + '0';
+	write(fd, &c, 1);
+}
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	size_t	i;
+
+	i = 0;
+	if (s == NULL)
+		return ;
+	while (s[i] != '\0')
+	{
+		write(fd, &s[i], 1);
+		i++;
+	}
+}
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+
